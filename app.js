68c765d1279cdaf9aa207729c44cb168987ed0c5
Yasir384 <yasiryaseen206@gmail.com>
2025-01-30 06:20:20 +0500

    update and delete from firestore is working

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-import {
-  collection,
-  addDoc,
-  getDocs,
-} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import {collection, addDoc, getDocs,query,orderBy,Timestamp, doc, deleteDoc,updateDoc} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 import { db } from "./config.js";
 
 const form = document.querySelector("#form");
 const tittle = document.querySelector("#tittle");
 const description = document.querySelector("#description");
 const div = document.querySelector(".container");
+
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -18,10 +15,13 @@ form.addEventListener("submit", async (event) => {
     const docRef = await addDoc(collection(db, "todos"), {
       tittle: tittle.value,
       description: description.value,
+      time: Timestamp.fromDate(new Date())
     });
     console.log("Document written with ID: ", docRef.id);
     tittle.value = "";
     description.value = "";
+    getData()
+   
     
   } catch (e) {
     console.error("Error adding document: ", e);
@@ -30,7 +30,9 @@ form.addEventListener("submit", async (event) => {
 
 const arr = [];
 async function getData() {
-  const querySnapshot = await getDocs(collection(db, "todos"));
+  arr.length=0;
+  const q = query(collection(db, "todos"), orderBy("time", "asc"));
+  const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     arr.push({ id: doc.id, ...doc.data() }); 
   });
@@ -43,12 +45,50 @@ function renderData() {
   arr.forEach((item) => {
     div.innerHTML += `
     <div class="box">
-            <p>id: ${item.id}</p>
-            <p>Title: ${item.tittle}</p>
-            <p>Description: ${item.description}</p>
+    <p>Title: ${item.tittle}</p>
+    <p>Description: ${item.description}</p>
+    <p>Time: ${new Date(item.time.seconds * 1000).toLocaleString()}</p>
+    <div class="stylebtn">
+    <button class="delBtn">delete</button>
+    <button class="editBtn">edit</button>
+    </div>
     </div>`
   });
+  const delBtn=document.querySelectorAll('.delBtn');
+  const editBtn=document.querySelectorAll('.editBtn');
+
+  delBtn.forEach((btnn , index)=>{
+    btnn.addEventListener('click', async () => {
+      console.log(arr[index]);
+      await deleteDoc(doc(db, "todos", arr[index].id));
+      console.log("Data deleted");
+      arr.splice(index, 1);
+      renderData();
+      // console.log(arr);
+      
+      
+    })
+    
+  })
+
+  editBtn.forEach((edit,index)=>{
+    edit.addEventListener('click',async () => {
+      console.log(arr[index]);
+      const updatedNewValue = prompt("enter new new description");
+      const todoUpdated = doc(db, "todos", arr[index].id);
+      await updateDoc(todoUpdated, { description:updatedNewValue});
+      console.log("update todo");
+      arr[index].description = updatedNewValue;
+      renderData();
+      
+      
+    })
+  })
 }
 
 getData();
+
+
+
+
 
